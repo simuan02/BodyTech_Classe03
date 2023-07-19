2be9b8c0e6dbcc17100297bf80d0330e2869fa44
JacopoDeveloper <jacopodedominicisdeveloper@gmail.com>
2023-07-19 18:24:20 +0200

    Aggiunta Entity Class Esercizio

--- src/main/java/bodyTech/model/entity/Esercizio.java
+++ src/main/java/bodyTech/model/entity/Esercizio.java
@@ -1,4 +1,23 @@
 package bodyTech.model.entity;
 
 public class Esercizio {
+
+    public String getNomeEsercizio() {
+        return nomeEsercizio;
+    }
+
+    public void setNomeEsercizio(String nomeEsercizio) {
+        this.nomeEsercizio = nomeEsercizio;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    private String nomeEsercizio;
+    private String descrizione;
 }
