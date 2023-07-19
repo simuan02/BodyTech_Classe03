ccfd64c0c74f2548d86b7df8c8cfa9e509bfa20f
Simone <simone2002esposito@gmail.com>
2023-07-19 10:26:11 +0200

    ConnectionPool

---
+++ src/main/java/bodyTech/model/ConPool.java
@@ -0,0 +1,30 @@
+package bodyTech.model;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+import java.util.TimeZone;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConPool {
+    private static DataSource datasource;
+
+    public static Connection getConnection() throws SQLException {
+        if (datasource == null) {
+            PoolProperties p = new PoolProperties();
+            p.setUrl("jdbc:mysql://localhost:3306/bodytechdb?serverTimezone=" + TimeZone.getDefault().getID());
+            p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            p.setUsername("root");
+            p.setPassword("password");
+            p.setMaxActive(100);
+            p.setInitialSize(10);
+            p.setMinIdle(10);
+            p.setRemoveAbandonedTimeout(60);
+            p.setRemoveAbandoned(true);
+            datasource = new DataSource();
+            datasource.setPoolProperties(p);
+        }
+        return datasource.getConnection();
+    }
+}
