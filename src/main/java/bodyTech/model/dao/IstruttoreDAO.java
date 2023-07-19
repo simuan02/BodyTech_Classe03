c143abad0c8c3a009dff20dc845db9975049921f
Simone <simone2002esposito@gmail.com>
2023-07-19 18:01:57 +0200

    Modifica Codice Entità Istruttore e SchedaAllenamento

--- src/main/java/bodyTech/model/dao/IstruttoreDAO.java
+++ src/main/java/bodyTech/model/dao/IstruttoreDAO.java
@@ -1,4 +1,12 @@
 package bodyTech.model.dao;
 
+import bodyTech.model.entity.Istruttore;
+
+import java.util.List;
+
 public class IstruttoreDAO {
+
+    public static List<Istruttore> visualizzaIstruttori(){
+    return null;
+    }
 }
