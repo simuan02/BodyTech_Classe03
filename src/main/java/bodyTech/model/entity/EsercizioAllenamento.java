9a6a46cc8453cecd90377f9bd3f80f0e6ba23f80
JacopoDeveloper <jacopodedominicisdeveloper@gmail.com>
2023-07-19 18:27:19 +0200

    Aggiunta Entity Class EsercizioAllenamento

--- src/main/java/bodyTech/model/entity/EsercizioAllenamento.java
+++ src/main/java/bodyTech/model/entity/EsercizioAllenamento.java
@@ -1,4 +1,14 @@
 package bodyTech.model.entity;
 
 public class EsercizioAllenamento {
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    private String volume;
 }
