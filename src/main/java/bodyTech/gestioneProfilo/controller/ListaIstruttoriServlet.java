1d178d94d9348a0e563c4069a93ffb95bd572eaa
Simone <simone2002esposito@gmail.com>
2023-09-10 00:26:52 +0200

    Inserimento di nuove funzionalità nella gestione degli Istruttori da parte di Amministratore

--- src/main/java/bodyTech/gestioneProfilo/controller/ListaIstruttoriServlet.java
+++ src/main/java/bodyTech/gestioneProfilo/controller/ListaIstruttoriServlet.java
@@ -3,7 +3,6 @@ package bodyTech.gestioneProfilo.controller;
 import bodyTech.gestioneProfilo.service.GestioneProfiloService;
 import bodyTech.gestioneProfilo.service.GestioneProfiloServiceImpl;
 import bodyTech.model.dao.SchedaAllenamentoDAO;
-import bodyTech.model.dao.UtenteDAO;
 import bodyTech.model.entity.Istruttore;
 import bodyTech.model.entity.Profilo;
 import bodyTech.model.entity.SchedaAllenamento;
@@ -21,39 +20,22 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebServlet(name = "ListaUtentiServlet", urlPatterns = {"/listaUtenti"})
-public class ListaUtentiServlet extends HttpServlet {
+@WebServlet(name = "ListaIstruttoriServlet", urlPatterns = {"/listaIstruttori"})
+public class ListaIstruttoriServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         Profilo p = (Profilo) session.getAttribute("Profilo");
         try {
             GestioneProfiloService services = new GestioneProfiloServiceImpl();
-            List<Utente> listaUtenti = services.visualizzaUtenti(p);
-            if (p.loggedUserLevel().equals("Istruttore")) {
-                List<Utente> listaTuoiAssociati = new ArrayList<>();
-                List<Utente> listaAssociati = new ArrayList<>();
-                List<Utente> listaNonAssociati = new ArrayList<>();
-                for (Utente u : listaUtenti) {
-                    SchedaAllenamento scheda = SchedaAllenamentoDAO.findSchedaByUtente(u.getCodiceFiscale());
-
-                    if (scheda != null) {
-                        if (scheda.getIstruttore().getMatricolaIstruttore().equals(((Istruttore)p).getMatricolaIstruttore()))
-                            listaTuoiAssociati.add(u);
-                        else listaAssociati.add(u);
-                    } else
-                        listaNonAssociati.add(u);
-                }
-                request.setAttribute("listaTuoiAssociati", listaTuoiAssociati);
-                request.setAttribute("listaAssociati", listaAssociati);
-                request.setAttribute("listaNonAssociati", listaNonAssociati);
-            } else if (p.loggedUserLevel().equals("Amministratore")) {
-                request.setAttribute("listaUtenti", listaUtenti);
+            List<Istruttore> listaIstruttori = services.visualizzaIstruttori(p);
+            if (p.loggedUserLevel().equals("Amministratore")) {
+                request.setAttribute("listaIstruttori", listaIstruttori);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/listaIstruttoriPage.jsp");
+                dispatcher.forward(request, response);
             }
             else
                 response.sendError(403, "Operazione non consentita!");
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/listaUtentiPage.jsp");
-            dispatcher.forward(request, response);
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (RuntimeException e2){
