cd99d669e58915daad58c8e2bbf74191fd562369
Simone <simone2002esposito@gmail.com>
2023-10-25 16:53:58 +0200

    Aggiunta Casi di test RegistrazioneController e modifica Struttura Sottosistema GestioneProfiloController

--- src/main/java/bodyTech/gestioneProfilo/controller/VisualizzaSchedaServlet.java
+++ src/main/java/bodyTech/gestioneProfilo/controller/VisualizzaSchedaServlet.java
@@ -1,22 +1,12 @@
 package bodyTech.gestioneProfilo.controller;
 
-import bodyTech.gestioneProfilo.service.ProfiloService;
-import bodyTech.gestioneProfilo.service.ProfiloServiceImpl;
-import bodyTech.model.dao.SchedaAllenamentoDAO;
-import bodyTech.model.dao.UtenteDAO;
-import bodyTech.model.entity.Profilo;
-import bodyTech.model.entity.SchedaAllenamento;
-import bodyTech.model.entity.Utente;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 /**
  * Questa servlet consente la ricerca nel DB della scheda associata al Profilo corrente,
@@ -31,24 +21,7 @@ public class VisualizzaSchedaServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        HttpSession session = request.getSession();
-        Profilo p = (Profilo) session.getAttribute("Profilo");
-
-        try{
-            ProfiloService services = new ProfiloServiceImpl();
-            SchedaAllenamento sa = services.visualizzaScheda(p);
-            if (sa != null)
-                request.setAttribute("SchedaAllenamento", sa);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            response.sendError(400, "Errore nella richiesta");
-        }
-        catch (RuntimeException e2) {
-            response.sendError(403, "Richiesta non autorizzata");
-        }
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/scheda.jsp");
-        dispatcher.forward(request, response);
+        GestioneProfiloController.visualizzaSchedaMethod(request, response);
     }
 
     @Override
--- src/main/java/bodyTech/registrazione/controller/RegistrazioneController.java
+++ src/main/java/bodyTech/registrazione/controller/RegistrazioneController.java
@@ -32,27 +32,7 @@ public class RegistrazioneController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String nome = request.getParameter("Nome");
-        String cognome = request.getParameter("Cognome");
-        String codiceFiscale = request.getParameter("Identifier");
-        String password = request.getParameter("password");
-        Utente u = new Utente();
-        u.setCodiceFiscale(codiceFiscale);
-        u.setNome(nome);
-        u.setCognome(cognome);
-        u.setPassword(password);
-        RegistrazioneService services = new RegistrazioneServiceImpl();
-        List<String> problemiRegistrazione = services.registrazioneUtente(u, password);
-        if (problemiRegistrazione.isEmpty()) {
-            request.setAttribute("Registrazione", true);
-        } else {
-            request.setAttribute("Registrazione", false);
-            for (String problema : problemiRegistrazione) {
-                String problemaNoSpaces = problema.replaceAll(" ", "");
-                request.setAttribute(problemaNoSpaces, problema);
-            }
-        }
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
-        dispatcher.forward(request, response);
+        RegistrazioneControllerImpl registrazioneController = new RegistrazioneControllerImpl();
+        registrazioneController.userRegistration(request, response);
     }
 }
